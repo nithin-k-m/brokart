ffe770858ae7e16cc6a4382a4cc3476d49e149b4
Nithin K Manuel <garfielda476@gmail.com>
2026-02-19 16:30:08 +0530

    Rename project from brocart to brokart and update imports

--- brokart/asgi.py
+++ brokart/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for brocart project.
+ASGI config for brokart project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brocart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brokart.settings')
 
 application = get_asgi_application()
--- brokart/urls.py
+++ brokart/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for brocart project.
+URL configuration for brokart project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
